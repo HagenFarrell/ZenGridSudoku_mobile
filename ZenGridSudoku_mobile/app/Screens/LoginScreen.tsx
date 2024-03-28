f71824f9ca5a38ab454e535feb3ad2d1285d454a
Santos-Matthew <matthew.e.santos47@gmail.com>
2024-03-28 18:04:26 -0400

    Hagen's Login

--- ZenGridSudoku_mobile/app/Screens/LoginScreen.tsx
+++ ZenGridSudoku_mobile/app/Screens/LoginScreen.tsx
@@ -5,18 +5,83 @@
  * 
 */
 
-import { useNavigation } from 'expo-router';
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import axios, { AxiosError } from 'axios';
+import React, { useState } from 'react';
+import { View, Text, Button, Alert, StyleSheet, TextInput } from 'react-native';
 
 // Currently trying to figure out the proper type
-const LoginScreen = ({navigation}: any) => {
+const LoginScreen = ({ navigation }: any) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginPress = async () => {
+    try {
+      const response = await axios.post(
+        "http://sudokuapp-f0e20225784a.herokuapp.com/api/login",
+        {
+          username: username,
+          password: password,
+        }
+      );
+
+      console.log("Login successful:", response.data);
+      Alert.alert("Login Success", "You have logged in successfully!");
+      navigation.goBack();
+      // Navigate or update state as needed here
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      if (axiosError.response) {
+        // Assuming the response.data is an object with a message property
+        const message = (axiosError.response.data as { message: string })
+          .message;
+        Alert.alert("Login Failed", message || "An unexpected error occurred");
+      }
+      // Handle other error cases
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text onPress={() => {navigation.goBack()}} >Login Screen (Click me to go back)</Text>
-      <Button title="No Account? Clicke me to Sign Up!" onPress={() => navigation.navigate("RegisterScreen")} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLoginPress} />
+      <Button title="Tempback" onPress={() => navigation.goBack("HomeScreen")}></Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+});
 
 export default LoginScreen;
--- ZenGridSudoku_mobile/app/Screens/RegisterScreen.tsx
+++ ZenGridSudoku_mobile/app/Screens/RegisterScreen.tsx
@@ -5,15 +5,13 @@
  * 
 */
 
-import { useNavigation } from 'expo-router';
 import React from 'react';
 import { View, Text } from 'react-native';
 
 // Currently trying to figure out the proper type
-const RegisterScreen = ({navigation}: any) => {
+const RegisterScreen = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text onPress={() => {navigation.goBack()}} >Register Screen (Click me to go back)</Text>
     </View>
   );
 }
