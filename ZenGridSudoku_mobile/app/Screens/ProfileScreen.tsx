e830ea0e675a5dd79a6343a74df9caa86151cfd8
HagenFarrell <hagen.farrell@gmail.com>
2024-04-22 10:35:47 -0400

    [Style] Saving changes

--- ZenGridSudoku_mobile/app/Screens/ProfileScreen.tsx
+++ ZenGridSudoku_mobile/app/Screens/ProfileScreen.tsx
@@ -1,9 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import { useAuth } from "../Navigation/AuthContext";
 import axios from "axios";
 import { useFocusEffect } from "@react-navigation/native";
+import { BlurView } from 'expo-blur';
 
 const ProfileScreen: React.FC = () => {
   const [userInfo, setUserInfo] = useState({
@@ -67,29 +68,47 @@ const ProfileScreen: React.FC = () => {
   );
 
   return (
-    <View style={styles.container}>
-      {isLoading && <Text>Loading...</Text>}
-      {error && <Text style={styles.error}>Error: {error}</Text>}
-      {!isLoading && !error && (
-        <>
-          <Text style={styles.heading}>Welcome, {userInfo.username}</Text>
-          <Text>Your User ID: {userInfo.userId}</Text>
-          <Text>Your email: {userInfo.email}</Text>
-          <Text style={styles.statsHeading}>Puzzle Stats:</Text>
-          <Text>Easy Puzzles Solved: {stats.easy}</Text>
-          <Text>Medium Puzzles Solved: {stats.medium}</Text>
-          <Text>Hard Puzzles Solved: {stats.hard}</Text>
-        </>
-      )}
+    <View style={styles.backgroundContainer}>
+      <BlurView
+        style={styles.backgroundImage}
+        intensity={80}
+        tint="light"
+      >
+        <Image
+          style={styles.backgroundImage}
+          source={require("../imgs/background.jpg")}
+        />
+      </BlurView>
+      <View style={styles.container}>
+        {isLoading && <Text>Loading...</Text>}
+        {error && <Text style={styles.error}>Error: {error}</Text>}
+        {!isLoading && !error && (
+          <>
+            <Text style={styles.heading}>Welcome, {userInfo.username}</Text>
+            <Text style={styles.statsHeading}>Puzzle Stats:</Text>
+            <View style={styles.statRow}>
+              <View style={[styles.statContainer, styles.easy]}>
+                <Text>Easy: {stats.easy + "/50"}</Text>
+              </View>
+              <View style={[styles.statContainer, styles.medium]}>
+                <Text>Medium: {stats.medium + "/50"}</Text>
+              </View>
+              <View style={[styles.statContainer, styles.hard]}>
+                <Text>Hard: {stats.hard + "/50"}</Text>
+              </View>
+            </View>
+          </>
+        )}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0,
+    width: "80%",
     justifyContent: "center",
-    alignItems: "center",
     padding: 20,
   },
   error: {
@@ -97,14 +116,55 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   heading: {
-    fontSize: 22,
+    fontSize: 25,
     fontWeight: "bold",
     marginBottom: 10,
   },
   statsHeading: {
+    fontWeight: "bold",
     marginTop: 20,
     fontSize: 18,
     marginBottom: 5,
+  },
+  statRow: {
+    flexDirection: "row", // Arrange child elements horizontally
+    height: "25%",
+    width: "40%",
+    justifyContent: "space-between", // Distribute containers evenly
+  },
+  statContainer: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 50, // Adjust for squared corners or use 40 for a circle
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  easy: {
+    backgroundColor: "green",
+  },
+  medium: {
+    backgroundColor: "yellow",
+  },
+  hard: {
+    backgroundColor: "#d94c38",
+  },
+  // Add these to your existing styles object
+  backgroundContainer: {
+    flex: 1,
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  profileContent: {
+    flex: 1, // Allow content to potentially take up the remaining space
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
   },
 });
 
