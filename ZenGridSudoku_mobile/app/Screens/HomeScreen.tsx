1f06fd73b60b3ca2a463b8b554da3d9f361ec71c
HagenFarrell <hagen.farrell@gmail.com>
2024-04-25 16:31:05 -0400

    Styling update

--- ZenGridSudoku_mobile/app/Screens/HomeScreen.tsx
+++ ZenGridSudoku_mobile/app/Screens/HomeScreen.tsx
@@ -1,26 +1,37 @@
+/*
+ *
+ *
+ *
+ */
 
-/* 
- * 
- * 
- * 
-*/
-
-import { Link } from 'expo-router';
-import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
-import LoginScreen from './LoginScreen';
-import SudokuBoard from '@/components/Sudoku/SudokuBoard';
-import Sudoku from '@/components/Sudoku/Sudoku';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import RegisterScreen from './RegisterScreen';
-import PuzzleSelect from './PuzzleSelect';
+import { Link } from "expo-router";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+} from "react-native";
+import LoginScreen from "./LoginScreen";
+import SudokuBoard from "@/components/Sudoku/SudokuBoard";
+import Sudoku from "@/components/Sudoku/Sudoku";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import RegisterScreen from "./RegisterScreen";
+import PuzzleSelect from "./PuzzleSelect";
 
 //
-const empty_test = "000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-const invalid_test = "111111111111111111111111111111111111111111111111111111111111111111111111111111110"
-const dev_test = "158723469367954821294816375619238540485690132732145986976381254841572693523469718"
-const one_test = "132567948546389217978241635264918753715602894389475126857123469691754382423896571"
-const wrap_test = "123456789123456789123456789123456789123456789123456789123456789123456789123456780"
+const empty_test =
+  "000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+const invalid_test =
+  "111111111111111111111111111111111111111111111111111111111111111111111111111111110";
+const dev_test =
+  "158723469367954821294816375619238540485690132732145986976381254841572693523469718";
+const one_test =
+  "132567948546389217978241635264918753715602894389475126857123469691754382423896571";
+const wrap_test =
+  "123456789123456789123456789123456789123456789123456789123456789123456789123456780";
 //                                                             ^
 
 /*
@@ -38,134 +49,103 @@ const wrap_test = "1234567891234567891234567891234567891234567891234567891234567
 /* <Sudoku type='easy' puzzle={-1} init={puzzle}></Sudoku> */
 
 const HomeScreen = ({ navigation }: any) => {
-  const gotoPuzzleSelectScreen = () => navigation.navigate(PuzzleSelect.name)
+  const gotoPuzzleSelectScreen = () => navigation.navigate(PuzzleSelect.name);
   const gotoLoginScreen = () => navigation.navigate(LoginScreen.name);
-  const gotoRegisterScreen = () => navigation.navigate(RegisterScreen.name)
+  const gotoRegisterScreen = () => navigation.navigate(RegisterScreen.name);
 
-  const puzzleSelectDisplay = "Puzzle Select [undefined]"
-  const loginDisplay = "Login"
-  const registerDisplay = "Signup"
+  const puzzleSelectDisplay = "Lets play Sudoku!";
+  const loginDisplay = "Login";
+  const registerDisplay = "Signup";
 
-  const [puzzle, setPuzzle] = useState<string>(dev_test)
+  const [puzzle, setPuzzle] = useState<string>(dev_test);
 
   return (
-    <View style={styles.container}>
+    <ImageBackground
+      source={require("../imgs/background.jpg")}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>ZenGridSudoku</Text>
+        </View>
+        {/* Navigate to PuzzleSelectScreen*/}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={gotoPuzzleSelectScreen}
+        >
+          <Text style={styles.buttonText}>{puzzleSelectDisplay}</Text>
+        </TouchableOpacity>
 
-      {/* Navigate to PuzzleSelectScreen*/}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={gotoPuzzleSelectScreen}
-      >
-        <Text style={styles.buttonText}>{puzzleSelectDisplay}</Text>
-      </TouchableOpacity>
+        {/*Navigation to LoginScreen*/}
+        <TouchableOpacity
+          testID="loginButton"
+          style={styles.button}
+          onPress={gotoLoginScreen}
+        >
+          <Text style={styles.buttonText}>{loginDisplay}</Text>
+        </TouchableOpacity>
 
-      {/*Navigation to LoginScreen*/}
-      <TouchableOpacity
-        testID="loginButton"
-        style={styles.button}
-        onPress={gotoLoginScreen}
-      >
-        <Text style={styles.buttonText}>{loginDisplay}</Text>
-      </TouchableOpacity>
-
-      {/* Navigation to RegisterScreen*/}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={gotoRegisterScreen}
-      >
-        <Text style={styles.buttonText}>{registerDisplay}</Text>
-      </TouchableOpacity>
-
-    </View>
+        {/* Navigation to RegisterScreen*/}
+        <TouchableOpacity style={styles.button} onPress={gotoRegisterScreen}>
+          <Text style={styles.buttonText}>{registerDisplay}</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "white",
-    color: "black",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "black",
-    padding: 10,
-    alignItems: "center", // Center text horizontally
-    justifyContent: "center", // Center text vertically
-  },
-  buttonText: {
-    color: "black", // Ensures the text color is white
-    fontSize: 16, // You can adjust the size as needed
-  },
-  backgroundImage: {
+  container: {
+    width:"100%",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  inputContainer: {
-    width: "100%", // Or a fixed width in pixels
-    backgroundColor: "rgba(255, 255, 255, 0.7)",
-    borderRadius: 5,
-    padding: 10,
-    // Ensure there's no flex property causing dynamic changes in size
+  header: {
+    padding: 20,
+    marginTop: 0,
+    backgroundColor: "#4CAF50", // Use the primary color for coherence
+    width: "100%", // Full width to stretch across the screen
+    alignItems: "center", // Center align text
+    marginBottom: 30, // Spacing from the top element
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 5,
   },
-  input: {
-    backgroundColor: "rgba(255, 255, 255, 0.9)", // More opaque white for inputs
-    borderRadius: 5,
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 10,
-    width: "100%", // Ensures the input stretches to fill the container
-    height: 50, // Fixed height for all inputs
-  },
-  title: {
-    color: "black",
+  headerText: {
+    color: "white",
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 20,
   },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  button: {
+    backgroundColor: "#4CAF50", // A more vibrant green color for action buttons
+    borderRadius: 20,
+    borderWidth: 2, // Remove border for a cleaner look
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    marginVertical: 10, // Adds space between buttons
+    width: "80%", // Ensures buttons are uniformly sized
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3, // Adds subtle elevation for material design feel
   },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 20,
-    width: "65%",
+  buttonText: {
+    color: "white", // White text for better readability on green background
+    fontSize: 18, // Slightly larger font size
+    fontWeight: "bold",
+    textAlign: "center", // Ensures text is centered within the button
   },
-  Text: {
-    color: "white",
-    fontSize: 22,
-    textAlign: "center",
-  },
-  errorBanner: {
+  backgroundImage: {
     position: "absolute",
     top: 0,
     left: 0,
+    bottom: 0,
     right: 0,
-    height: 50,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  errorText: {
-    color: "white",
-    fontWeight: "bold",
   },
 });
-
-// This older style can properly center the sudoku board
-const styles_old = StyleSheet.create({
-  container: {
-    flex: 1,
-
-    alignItems: 'center',
-    justifyContent: 'center',
-
-    // Debug, comment out
-    // borderColor: 'green',
-    // borderWidth: 10,
-  }
-})
 
 export default HomeScreen;
--- ZenGridSudoku_mobile/app/Screens/PuzzleSelect.tsx
+++ ZenGridSudoku_mobile/app/Screens/PuzzleSelect.tsx
@@ -1,5 +1,12 @@
-
-import { Dimensions, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import PlayScreen from "./PlayScreen";
 import { useEffect, useState } from "react";
 import axios, { AxiosError } from "axios";
@@ -7,7 +14,7 @@ import SudokuContext from "../Navigation/SudokuContext";
 
 const PuzzleSelect = ({ navigation }: any) => {
   // Possibly change to let statements
-  const [type, setType] = useState<('easy' | 'medium' | 'hard')>('easy');
+  const [type, setType] = useState<"easy" | "medium" | "hard">("easy");
   const [puzzle, setPuzzle] = useState<string>("1");
 
   (global as any).typeCtx = "";
@@ -17,76 +24,67 @@ const PuzzleSelect = ({ navigation }: any) => {
   useEffect(() => {
     (global as any).typeCtx = "";
     (global as any).puzzleCtx = "";
-  }, [type, puzzle])
+  }, [type, puzzle]);
 
-  const gotoPlayScreen = () => navigation.navigate(PlayScreen.name)
+  const gotoPlayScreen = () => navigation.navigate(PlayScreen.name);
 
   // Sanitize and validate input
   const handleInput = (input: string) => {
-    if (input.length > 2)
-      return
+    if (input.length > 2) return;
 
     for (const [str] of input) {
-      if (str < '1' || str > '9')
-        return
+      if (str < "1" || str > "9") return;
     }
 
-    if (input == '')
-      setPuzzle("1")
+    if (input == "") setPuzzle("1");
 
-    const val = parseInt(input)
+    const val = parseInt(input);
 
-    if (val < 1 || val > 50)
-      return
+    if (val < 1 || val > 50) return;
 
-    setPuzzle(input)
-  }
+    setPuzzle(input);
+  };
 
   // Random Puzzle
-  const types: ('easy' | 'medium' | 'hard')[] = ["easy", "medium", "hard"]
+  const types: ("easy" | "medium" | "hard")[] = ["easy", "medium", "hard"];
   const randomPuzzle = () => {
-    const type: number = Math.floor(Math.random() * 3)
-    const puzzle = (Math.floor(Math.random() * 49) + 1).toString()
+    const type: number = Math.floor(Math.random() * 3);
+    const puzzle = (Math.floor(Math.random() * 49) + 1).toString();
 
-    setType(types[type])
-    setPuzzle(puzzle)
-  }
+    setType(types[type]);
+    setPuzzle(puzzle);
+  };
 
   // CURRENTLY DEBUG, either delete or repurpose?
-  useEffect(() => undefined, [type, puzzle])
+  useEffect(() => undefined, [type, puzzle]);
 
   const resolveAPI = () => {
     switch (type) {
       case "easy":
-        return "http://sudokuapp-f0e20225784a.herokuapp.com/api/getpuzzle_easy"
+        return "http://sudokuapp-f0e20225784a.herokuapp.com/api/getpuzzle_easy";
       case "medium":
-        return "http://sudokuapp-f0e20225784a.herokuapp.com/api/getpuzzle_medium"
+        return "http://sudokuapp-f0e20225784a.herokuapp.com/api/getpuzzle_medium";
       case "hard":
-        return "http://sudokuapp-f0e20225784a.herokuapp.com/api/getpuzzle_hard"
+        return "http://sudokuapp-f0e20225784a.herokuapp.com/api/getpuzzle_hard";
     }
-  }
+  };
 
   const requestPuzzle = async () => {
     try {
-      const response = await axios.post(
-        resolveAPI(),
-        {
-          puzzle_number: parseInt(puzzle)
-        }
-      );
+      const response = await axios.post(resolveAPI(), {
+        puzzle_number: parseInt(puzzle),
+      });
 
-      (global as any).initCtx = response.data.puzzlestring
-
-
+      (global as any).initCtx = response.data.puzzlestring;
 
       // If all goes well
-      gotoPlayScreen()
-    }
-    catch (error) {
+      gotoPlayScreen();
+    } catch (error) {
       const axiosError = error as AxiosError;
       if (axiosError.response) {
         // Assuming the response.data is an object with a message property
-        const message = (axiosError.response.data as { message: string }).message
+        const message = (axiosError.response.data as { message: string })
+          .message;
         handleError(message);
       }
     }
@@ -94,130 +92,129 @@ const PuzzleSelect = ({ navigation }: any) => {
 
   // DEBUG
   const handleError = (error: string) => {
-    alert(error)
-  }
+    alert(error);
+  };
 
   return (
+    <ImageBackground
+      source={require("../imgs/background.jpg")}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        {/* Difficulty Select */}
+        <View style={styles.website}>
+          <Text style={styles.title}>Difficulty:</Text>
 
-    <View style={styles.container}>
-      {/* DEBUG */}
-      <View>
-        <Text>DEBUG:</Text>
-        <Text>{type}</Text>
-        <Text>{puzzle}</Text>
-      </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setType("easy")}
+          >
+            <Text style={styles.font}>Easy</Text>
+          </TouchableOpacity>
 
-      {/* Difficulty Select */}
-      <View style={styles.website}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setType("medium")}
+          >
+            <Text style={styles.font}>Medium</Text>
+          </TouchableOpacity>
 
-        <Text>
-          Difficulty:
-        </Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => setType("hard")}
+          >
+            <Text style={styles.font}>Hard</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => setType('easy')}
-        >
-          <Text style={styles.font}>Easy</Text>
-        </TouchableOpacity>
+        {/* Puzzle Number */}
+        <View style={styles.website}>
+          <Text style={styles.title}>Puzzle:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="puzzle 1-50"
+            value={puzzle}
+            onChangeText={handleInput}
+            keyboardType="numeric"
+          />
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => setType('medium')}
-        >
-          <Text style={styles.font}>Medium</Text>
-        </TouchableOpacity>
+          {/* Random Button */}
+          <TouchableOpacity style={styles.button} onPress={randomPuzzle}>
+            <Text style={styles.font}>Random Puzzle</Text>
+          </TouchableOpacity>
+        </View>
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => setType('hard')}
-        >
-          <Text style={styles.font}>Hard</Text>
-        </TouchableOpacity>
-
-      </View>
-
-      {/* Puzzle Number */}
-      <View style={styles.website}>
-        <Text>Puzzle:</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="puzzle 1-50"
-          value={puzzle}
-          onChangeText={handleInput}
-          keyboardType="numeric"
-        />
-
-        {/* Random Button */}
-        <TouchableOpacity
-          style={styles.button}
-          onPress={randomPuzzle}
-        >
-          <Text style={styles.font}>Random Puzzle</Text>
+        {/* Play Button */}
+        <TouchableOpacity style={styles.button} onPress={requestPuzzle}>
+          <Text style={styles.font}>Start</Text>
         </TouchableOpacity>
       </View>
-
-      {/* Play Button */}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={requestPuzzle}
-      >
-        <Text style={styles.font}>Start</Text>
-      </TouchableOpacity>
-
-    </View>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
   container: {
     flex: 1,
-    padding: 10,
-
+    padding: 20,
     alignItems: 'center',
     justifyContent: 'center',
-
-    // Debug, comment out
-    borderColor: 'green',
-    borderWidth: 10,
-
-    width: Dimensions.get("screen").width,
-    height: Dimensions.get("screen").height
+    width: '100%',
+    height: '100%',
   },
   website: {
     flex: 1,
     margin: 10,
-
-    // Debug, comment out
-    backgroundColor: '#d4e7a7',
-    borderColor: '#d4e7a7',
-    borderWidth: 10,
+    backgroundColor: '#f0f0f0',
+    borderColor: '#ccc',
+    borderWidth: 1,
     borderRadius: 10,
     width: "90%",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   },
   input: {
-    backgroundColor: "rgba(255, 255, 255, 0.9)", // More opaque white for inputs
+    backgroundColor: "#fff",
     borderRadius: 5,
     borderWidth: 1,
-    padding: 10,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
     marginBottom: 10,
-    width: "100%", // Ensures the input stretches to fill the container
-    height: 50, // Fixed height for all inputs
+    width: "80%",
+    height: 50,
   },
   button: {
-    borderColor: '#abe189',
-    backgroundColor: '#abe189',
-    borderRadius: 10
-  },
-  play: {
-
+    backgroundColor: '#4CAF50',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginVertical: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
   },
   font: {
-
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   bold: {
-
+    fontWeight: 'bold',
+    fontSize: 18,
   }
-})
+});
+
 
 export default PuzzleSelect;
